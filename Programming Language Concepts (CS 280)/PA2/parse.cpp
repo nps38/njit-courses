dc016a45818b0c3d0eec3f2c4cf4d5e982866aa0
Neha Shirwalker <97172302+nps38@users.noreply.github.com>
2023-06-14 19:01:33 -0400

    Add files via upload

---
+++ Programming Language Concepts (CS 280)/PA2/parse.cpp
@@ -0,0 +1,148 @@
+/* Definitions and some functions implementations
+ * parse.cpp to be completed
+ * Programming Assignment 2
+ * Spring 2022
+*/
+
+#include "parse.h"
+
+
+map<string, bool> defVar;
+map<string, Token> SymTable;
+
+namespace Parser {
+	bool pushed_back = false;
+	LexItem	pushed_token;
+
+	static LexItem GetNextToken(istream& in, int& line) {
+		if( pushed_back ) {
+			pushed_back = false;
+			return pushed_token;
+		}
+		return getNextToken(in, line);
+	}
+
+	static void PushBackToken(LexItem & t) {
+		if( pushed_back ) {
+			abort();
+		}
+		pushed_back = true;
+		pushed_token = t;	
+	}
+
+}
+
+static int error_count = 0;
+
+int ErrCount()
+{
+    return error_count;
+}
+
+void ParseError(int line, string msg)
+{
+	++error_count;
+	cout << line << ": " << msg << endl;
+}
+
+
+//Stmt is either a WriteLnStmt, ForepeatStmt, IfStmt, or AssigStmt
+//Stmt = AssigStmt | IfStmt | WriteStmt | ForStmt 
+bool Stmt(istream& in, int& line) {
+	bool status;
+	//cout << "in ContrlStmt" << endl;
+	LexItem t = Parser::GetNextToken(in, line);
+	
+	switch( t.GetToken() ) {
+
+	case WRITELN:
+		status = WriteLnStmt(in, line);
+		//cout << "After WriteStmet status: " << (status? true:false) <<endl;
+		break;
+
+	case IF:
+		status = IfStmt(in, line);
+		break;
+
+	case IDENT:
+		Parser::PushBackToken(t);
+        status = AssignStmt(in, line);
+		
+		break;
+		
+	case FOR:
+		status = ForStmt(in, line);
+		
+		break;
+		
+		
+	default:
+		Parser::PushBackToken(t);
+		return false;
+	}
+
+	return status;
+}//End of Stmt
+
+
+//WriteStmt:= wi, ExpreList 
+bool WriteLnStmt(istream& in, int& line) {
+	LexItem t;
+	//cout << "in WriteStmt" << endl;
+	
+	t = Parser::GetNextToken(in, line);
+	if( t != LPAREN ) {
+		
+		ParseError(line, "Missing Left Parenthesis");
+		return false;
+	}
+	
+	bool ex = ExprList(in, line);
+	
+	if( !ex ) {
+		ParseError(line, "Missing expression after WriteLn");
+		return false;
+	}
+	
+	t = Parser::GetNextToken(in, line);
+	if(t != RPAREN ) {
+		
+		ParseError(line, "Missing Right Parenthesis");
+		return false;
+	}
+	//Evaluate: print out the list of expressions values
+
+	return ex;
+}
+
+
+//ExprList:= Expr {,Expr}
+bool ExprList(istream& in, int& line) {
+	bool status = false;
+	//cout << "in ExprList and before calling Expr" << endl;
+	status = Expr(in, line);
+	if(!status){
+		ParseError(line, "Missing Expression");
+		return false;
+	}
+	
+	LexItem tok = Parser::GetNextToken(in, line);
+	
+	if (tok == COMMA) {
+		//cout << "before calling ExprList" << endl;
+		status = ExprList(in, line);
+		//cout << "after calling ExprList" << endl;
+	}
+	else if(tok.GetToken() == ERR){
+		ParseError(line, "Unrecognized Input Pattern");
+		cout << "(" << tok.GetLexeme() << ")" << endl;
+		return false;
+	}
+	else{
+		Parser::PushBackToken(tok);
+		return true;
+	}
+	return status;
+}
+
+
